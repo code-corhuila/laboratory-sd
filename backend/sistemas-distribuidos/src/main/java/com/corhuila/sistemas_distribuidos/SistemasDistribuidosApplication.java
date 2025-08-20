534997aa77aa3f00a5e469e51e98850c43777594
Jesús Ariel González Bonilla <jesus.gonzalez@jikkosoft.com>
2025-08-19 20:00:03 -0500

    struct backend

---
+++ backend/sistemas-distribuidos/src/main/java/com/corhuila/sistemas_distribuidos/SistemasDistribuidosApplication.java
@@ -0,0 +1,13 @@
+package com.corhuila.sistemas_distribuidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasDistribuidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasDistribuidosApplication.class, args);
+	}
+
+}
